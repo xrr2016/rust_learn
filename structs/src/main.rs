002be41a0281376b9e332dd42cb9c336c9c06dd2
xiaoranran <xiaoranran1993@outlook.com>
2022-10-19 09:51:58 +0800

    feat: struct

---
+++ structs/src/main.rs
@@ -0,0 +1,26 @@
+fn main() {
+    let user1 = User {
+        email: String::from("someone@example.com"),
+        username: String::from("someusername123"),
+        active: true,
+        sign_in_count: 1,
+    };
+
+    println!("{}", user1.email);
+}
+
+fn build_user(email: String, username: String) -> User {
+    User {
+        email,
+        username,
+        active: true,
+        sign_in_count: 1,
+    }
+}
+
+struct User {
+    active: bool,
+    username: String,
+    email: String,
+    sign_in_count: u64,
+}
