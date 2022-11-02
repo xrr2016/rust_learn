1c68aaf3b1ec1d6043be6a5060ba4d148fc7d1df
xiaoranran <xiaoranran1993@outlook.com>
2022-11-02 19:30:16 +0800

    feat: update

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -7,9 +7,14 @@ fn main() {
 
     let mut index = String::new();
 
-    io::stdin().read_line(&mut index).expect("Failed to read line");
+    io::stdin()
+        .read_line(&mut index)
+        .expect("Failed to read line");
 
-    let index: usize = index.trim().parse().expect("Index entered was not a number");
+    let index: usize = index
+        .trim()
+        .parse()
+        .expect("Index entered was not a number");
 
     let element = a[index];
 
